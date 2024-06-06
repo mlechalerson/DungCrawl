bc851816266adb1fbdf69f10ef814a3fdcca961f
Michał Ruliński <michal.rulinskI@tm1.edu.pl>
2024-06-06 14:12:54 +0200

    create Entity, Player

--- src/main/java/org/example/GamePanel.java
+++ src/main/java/org/example/GamePanel.java
@@ -7,7 +7,7 @@ public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize = 16;
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale;
+    public final int tileSize = originalTileSize * scale;
     final int maxScreenRow = 12;
     final int maxScreenCol = 16;
     final int screenWidth = tileSize * maxScreenCol;
@@ -15,6 +15,8 @@ public class GamePanel extends JPanel implements Runnable{
     int FPS  = 60;
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    Player player = new Player(this,keyH);
 
     int playerX = 100;
     int playerY = 100;
@@ -100,22 +102,13 @@ public class GamePanel extends JPanel implements Runnable{
     }
     public void update(){
 
-        if(keyH.upPressed) {
-            playerY -= playerSpeed;
-        }else if (keyH.downPressed){
-            playerY += playerSpeed;
-        }else if (keyH.leftPressed){
-            playerX -= playerSpeed;
-        }else if (keyH.rightPressed){
-            playerX += playerSpeed;
-        }
+        player.update();
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D)g;
-        g2.setColor(Color.white);
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        player.draw(g2);
         g2.dispose();
     }
 }
